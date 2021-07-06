629fc8320a2bb1f0cb24da7d65f75189efe7d823
unknown <khadkauj@gmail.com>
2021-07-06 19:15:55 +0200

    hah

--- src/Components/Footer/FooterComponent.js
+++ src/Components/Footer/FooterComponent.js
@@ -2,13 +2,21 @@
 import { Typography } from "@material-ui/core";
 import React from "react";
 import { NavLink } from "react-router-dom";
-
+import IconButton from '@material-ui/core/IconButton';
+import InstagramIcon from '@material-ui/icons/Instagram';
 import "./FooterComponent.css";
 const FooterComponent = () => {
     return (
         <div className="footer">
-            <p className="footer__left">Whonung Gang</p>
-            <p className="footer_end">Infinity, The world is Infinite.</p>
+            <a href="https://www.instagram.com/infintyuj/" className="link__newsDecoration" >
+                <div className="footer__icon12" >
+                    <p className="footer__left">Infintyuj</p>
+                    <IconButton >
+                        <InstagramIcon color="error" />
+                    </IconButton>
+                </div>
+            </a>
+            <p className="footer_end">The world is Infinite.</p>
         </div>
     );
 };
