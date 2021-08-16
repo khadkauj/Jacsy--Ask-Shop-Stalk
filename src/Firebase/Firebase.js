034d06e44350db0f0a3001befa61581977f23ab1
unknown <khadkauj@gmail.com>
2021-08-16 21:26:13 +0200

    searching and sorting

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -21,21 +21,21 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
-firebase.firestore().enablePersistence()
-    .catch((err) => {
-        if (err.code == 'failed-precondition') {
-            console.log("firebase caching failed");
-            // Multiple tabs open, persistence can only be enabled
-            // in one tab at a a time.
-            // ...
-        } else if (err.code == 'unimplemented') {
-            console.log("firebase caching failed");
+// firebase.firestore().enablePersistence()
+//     .catch((err) => {
+//         if (err.code == 'failed-precondition') {
+//             console.log("firebase caching failed");
+//             // Multiple tabs open, persistence can only be enabled
+//             // in one tab at a a time.
+//             // ...
+//         } else if (err.code == 'unimplemented') {
+//             console.log("firebase caching failed");
 
-            // The current browser does not support all of the
-            // features required to enable persistence
-            // ...
-        }
-    });
+//             // The current browser does not support all of the
+//             // features required to enable persistence
+//             // ...
+//         }
+//     });
 // S
 
 
